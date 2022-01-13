c9930cca6d5595ffd822cd0ec1d86c8db8f18128
OlehPotap <potap.oleh.v@gmail.com>
2022-01-13 16:29:24 +0200

    task02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+galleryItems.forEach((el) => {
+  const newEl = document.createElement("li");
+  newEl.innerHTML = `<a class="gallery__item" href="${el.original}">
+<img
+        class="gallery__image"
+        src="${el.preview}"
+        alt="${el.description}"
+      />
+      </a>
+      `;
+  galleryEl.append(newEl);
+});
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
