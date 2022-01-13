abb96833dc78370e0b3a95c116d5aee51ba530b4
OlehPotap <potap.oleh.v@gmail.com>
2022-01-13 15:45:55 +0200

    task01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+galleryItems.forEach((el) => {
+  const newEl = document.createElement("div");
+  newEl.classList.add("gallery__item");
+  newEl.innerHTML = `<a class="gallery__link" href="${el.original}">
+<img
+        class="gallery__image"
+        src="${el.preview}"
+        data-source="${el.original}"
+        alt="${el.description}"
+      />
+      </a>
+      `;
+  galleryEl.append(newEl);
+});
+
+galleryEl.addEventListener("click", (event) => {
+  if (event.target.nodeName === "IMG") {
+    event.preventDefault();
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+    instance.show();
+
+    galleryEl.addEventListener("keydown", (event) => {
+      if (event.code === "Escape") {
+        instance.close();
+        galleryEl.removeEventListener("keydown", () => {});
+      }
+    });
+  }
+});
